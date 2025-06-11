5b8f850282d2a64582ee411d85f1b002ee1a3041
Tabban 121 <fortexsolution20@gmail.com>
2025-06-11 05:11:20 -0700

    new code added

--- src/middlewares/isAuthenticated.ts
+++ src/middlewares/isAuthenticated.ts
@@ -1,31 +1,25 @@
 import { Request, Response, NextFunction } from 'express';
-import jwt from 'jsonwebtoken';
 import { SessionRepository } from '../repositories/session.repository';
 
 const isAuthenticated = async (req: Request, res: Response, next: NextFunction): Promise<void> => {
   const authHeader = req.headers.authorization;
   const token = authHeader && authHeader.split(' ')[1];
+
   if (!token) {
     res.status(401).json({ message: 'No token provided' });
     return;
   }
-  // console.log('🧪 Raw token:', token);
-  try {
-    const decoded = jwt.verify(token, process.env.JWT_SECRET!) as { id: string; email: string };
 
-    const session = await SessionRepository.findByToken(token);
-    console.log('🧪 Found session:', session);
-    if (!session) {
-      res.status(401).json({ message: 'Session expired or invalid' });
-      return;
-    }
+  const session = await SessionRepository.findByToken(token);
+  console.log('🔍 Found session:', session);
 
-    console.log('✅ Valid token & session found:', token);
-    res.locals.userId = decoded.id;
-    next();
-  } catch (err) {
-    res.status(401).json({ message: 'Invalid token' });
+  if (!session) {
+    res.status(401).json({ message: 'Session expired or invalid' });
+    return;
   }
+
+  res.locals.userId = session.user.toString();
+  next();
 };
 
 export default isAuthenticated;
